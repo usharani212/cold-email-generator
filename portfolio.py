1c22f09ee76ce79f2469cb5156864a1527aa345d
P.Usharani <156448353+usharani212@users.noreply.github.com>
2025-07-06 23:30:18 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import chromadb
 import uuid
+from chromadb.config import Settings 
 
 class Portfolio:
-    def __init__(self, file_path="C:/code2/my_portfolioo.csv"):  # use forward slashes or raw string
+    def __init__(self, file_path="my_portfolioo.csv"):
         self.file_path = file_path
         self.data = pd.read_csv(file_path)
-        self.chroma_client = chromadb.Client()
-        self.collection = self.chroma_client.get_or_create_collection(name="portfolio")
+        self.chroma_client = chromadb.Client(
+            Settings(anonymized_telemetry=False)
+        )
 
+        self.collection = self.chroma_client.get_or_create_collection(name="portfolio")
     def load_portfolio(self):
         if not self.collection.count():
             for _, row in self.data.iterrows():
@@ -19,4 +22,4 @@ class Portfolio:
                 )
 
     def query_links(self, skills):
-        return self.collection.query(query_texts=skills, n_results=2).get('metadatas', [])
+        return self.collection.query(query_texts=skills, n_results=2).get("metadatas", [])
